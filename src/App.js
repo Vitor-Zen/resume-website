3008086982c4188404d58ddaef6ac195c7f47622
Vitor-Zen <vitorzen19@gmail.com>
2024-03-12 18:48:38 -0300

    save experience start point

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import ParticlesComponent from './components/Particles/Particles';
 import './App.css'
 
+// Think about how to improve these imports
 import Header from "./components/Header-component/index"
 import About from "./components/About-component/index"
+import Experience from "./components/Experience-component";
 import Projects from "./components/Projects-component";
-import Footer from "./components/Footer/index"
+import Footer from "./components/Footer-component/index";
 
 const App = () => {
     return (
       <div className="container">
         <Header/>
         <About/>
+        <Experience/>
         <Projects/>
         <Footer/>
         <ParticlesComponent />
---
+++ src/components/Experience-component/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Experience = () => {
+    return(
+        <div className="experience-container">
+            <h1>teste</h1>
+        </div>
+    );
+};
+
+export default Experience;
